d34375064d0d4240a475fca796c7a93d23893800
harphield <harphield@gmail.com>
2021-03-16 23:47:20 +0100

    Ukeire, updates, etc.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use crate::Cmd::{Shanten, Generate};
+use crate::Cmd::{Generate, Shanten, Ukeire};
 use riichi_tools_rs::riichi::hand::Hand;
-use structopt::StructOpt;
 use std::str::FromStr;
+use structopt::StructOpt;
+use serde_json::json;
 
 /// Riichi tools CLI
 #[derive(StructOpt, Debug)]
@@ -40,8 +41,8 @@ pub enum Cmd {
         #[structopt(short, long, default_value = "1")]
         number: i32,
     },
-    /// command C
-    C {},
+    /// Find ukeire of a hand
+    Ukeire { hand: String },
 }
 
 #[derive(Debug)]
@@ -54,43 +55,48 @@ impl FromStr for OutputType {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, String> {
-        match &s {
-            &"text" => Ok(OutputType::Text),
-            &"json" => Ok(OutputType::Json),
-            _ => Err("Wrong output type".to_string())
+        match s {
+            "text" => Ok(OutputType::Text),
+            "json" => Ok(OutputType::Json),
+            _ => Err("Wrong output type".to_string()),
         }
     }
 }
 
 fn main() {
     let opt = Opt::from_args();
-    // println!("{:#?}", opt);
     let verbose = opt.verbose;
     let output_type = opt.output_type;
 
     if let Some(command) = opt.cmd {
-        // println!("{:#?}", command);
-
         match command {
             Shanten { hand } => {
                 if let Ok(mut h) = Hand::from_text(&hand[..], true) {
                     match output_type {
-                        OutputType::Text => {
-                            match verbose {
-                                0 => println!("{}", h.shanten()),
-                                _ => println!("{}: {}", h.to_string(), h.shanten()),
-                            }
+                        OutputType::Text => match verbose {
+                            0 => println!("{}", h.shanten()),
+                            _ => println!("{}: {}", h.to_string(), h.shanten()),
                         },
-                        OutputType::Json => {
-                            
+                        OutputType::Json => match verbose {
+                            0 => {
+                                 println!("{{ {} }}", h.shanten());
+                            },
+                            _ => {
+                                let json_hand = json!({
+                                    "hand": h.to_string(),
+                                    "shanten": h.shanten()
+                                });
+                                println!("{}", json_hand);
+                            }
                         }
                     }
                 } else {
                     println!("invalid");
                 }
-            },
+            }
             Generate {
-                complete, number: count,
+                complete,
+                number: count,
             } => {
                 if complete {
                     match output_type {
@@ -98,9 +104,19 @@ fn main() {
                             for _i in 0..count {
                                 println!("{}", Hand::random_complete_hand(true, false));
                             }
-                        },
+                        }
                         OutputType::Json => {
+                            print!("{{ hands: [");
 
+                            for i in 0..count {
+                                print!(
+                                    "\"{}\"{} ",
+                                    Hand::random_complete_hand(true, false),
+                                    if i < count - 1 { "," } else { "" }
+                                );
+                            }
+
+                            println!("] }}");
                         }
                     }
                 } else {
@@ -109,14 +125,53 @@ fn main() {
                             for _i in 0..count {
                                 println!("{}", Hand::random_hand());
                             }
-                        },
+                        }
                         OutputType::Json => {
+                            print!("{{ hands: [");
 
+                            for i in 0..count {
+                                print!(
+                                    "\"{}\"{} ",
+                                    Hand::random_hand(),
+                                    if i < count - 1 { "," } else { "" }
+                                );
+                            }
+
+                            println!("] }}");
                         }
                     }
                 }
             }
-            _ => {}
+            Ukeire { hand } => {
+                if let Ok(mut h) = Hand::from_text(&hand[..], true) {
+                    let ukeire = h.find_shanten_improving_tiles(None);
+                    match output_type {
+                        OutputType::Text => {
+                            for (dis_tile_o, imp_tiles, value) in ukeire.iter() {
+                                match dis_tile_o {
+                                    None => {}
+                                    Some(dis_tile) => {
+                                        print!("{}: ", dis_tile);
+                                    }
+                                }
+
+                                print!("{} ", value);
+
+                                for (tile, cnt) in imp_tiles.iter() {
+                                    print!("{}({}) ", tile, cnt);
+                                }
+
+                                println!();
+                            }
+                        }
+                        OutputType::Json => {
+                            println!("{}", serde_json::to_string(&ukeire).unwrap());
+                        }
+                    }
+                } else {
+                    println!("invalid");
+                }
+            }
         }
     }
 }
